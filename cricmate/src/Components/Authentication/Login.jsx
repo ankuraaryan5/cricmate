707bb41c300547d31473d84242aabb289dc0d3b4
ankuraaryan5 <ankuraaryan5@gmail.com>
2024-10-20 11:12:45 +0530

    Styled Auth Components

--- cricmate/src/Components/Authentication/Login.jsx
+++ cricmate/src/Components/Authentication/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import axios from 'axios';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import {  FaCheckCircle } from "react-icons/fa";
 
 function Login() {
   const [data, setData] = useState({
@@ -33,11 +34,42 @@ function Login() {
   return (
     <div>
     <Navbar />
-    <form action="submit" className='flex flex-col gap-2 h-96 justify-center items-center w-full bg-slate-300'>
-      <input type="email" placeholder="Email" onChange={(e) => setData({ ...data, email: e.target.value })} value={data.email} className='bg-slate-200 p-2 rounded w-11/12 md:w-1/3 border border-black' />
-      <input type="password" placeholder='Password' onChange={(e) => setData({ ...data, password: e.target.value })} value={data.password} className='bg-slate-200 p-2 rounded w-11/12 md:w-1/3 border border-black' />
-      <button type="submit" onClick={handleLogin} className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-1/2 md:w-1/6'>Login</button>
+    <div className='flex flex-col md:flex-row justify-center items-center w-full bg-slate-300'>
+    <div className='flex flex-col justify-center items-center w-full md:w-1/2 relative h-screen'>
+    <img src="https://images.unsplash.com/photo-1708147684485-7d0485087be3?q=80&w=1968&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-full absolute" />
+        <div className="text-white flex flex-col  gap-2 absolute p-10">
+          <h1 className="text-3xl font-bold italic">Welcome to CricMate</h1>
+          <ul className="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4">
+                {[
+                  "Live Score Updates",
+                  "Competitive Analysis",
+                  "Ball-by-Ball Commentary",
+                  "Stats and Insights",
+                ].map((item, index) => (
+                  <li key={index} className="flex items-center space-x-3">
+                    <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
+                      <FaCheckCircle className="w-3.5 h-3.5 text-white" />
+                    </div>
+                    <span className="text-lg font-medium text-white">
+                      {item}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+          
+        </div>
+    </div>
+    <form action="submit" className='flex flex-col gap-2 justify-center items-center w-full md:w-1/2 py-10'>
+      <h1 className='text-2xl font-bold'>Login</h1>
+      <div className="flex gap-2 justify-center items-center ">
+        <p className="text-lg">Don't have an account? </p>
+        <Link to="/signup"> <p className="text-cyan-600 text-lg font-bold hover:text-orange-800">Join Now</p></Link>
+        </div>
+      <input type="email" placeholder="Email" onChange={(e) => setData({ ...data, email: e.target.value })} value={data.email} className='bg-slate-200 p-2 rounded w-11/12  border border-black' />
+      <input type="password" placeholder='Password' onChange={(e) => setData({ ...data, password: e.target.value })} value={data.password} className='bg-slate-200 p-2 rounded w-11/12 border border-black' />
+      <button type="submit" onClick={handleLogin} className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-1/2 '>Login</button>
     </form>
+    </div>
     <Footer />
     </div>
   )
--- cricmate/src/Components/Authentication/Signup.jsx
+++ cricmate/src/Components/Authentication/Signup.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import { Link } from "react-router-dom";
+import {  FaCheckCircle } from "react-icons/fa";
 
 function Signup() {
   const [data, setData] = useState({
@@ -32,6 +34,7 @@ function Signup() {
     e.preventDefault();
     if (!data.email || !data.password) {
       console.log("Input email and password");
+      alert("Input email and password");
       return;
     }
     setShowVerifyEmail(true);
@@ -41,6 +44,7 @@ function Signup() {
         { email: data.email }
       );
       console.log("OTP sent", response.data);
+      alert("OTP sent");
     } catch (error) {
       console.error("Error sending OTP:", error);
     }
@@ -52,28 +56,57 @@ function Signup() {
         email: data.email,
         otp: data.otp,
       });
-      console.log("OTP verified", response.data); 
-      // if (response.data.success) {
-      //   setShowVerifyEmail(false);
-      // }
+      console.log("OTP verified", response.data);
+
     } catch (error) {
-      console.error("Error verifying OTP:", error);
+      alert("Invalid OTP");
+      console.error( error);
     }
   };
   return (
     <>
       <Navbar />
-      <form className="flex flex-col gap-4 items-center justify-center h-96 bg-slate-200">
+      <div className="flex flex-col md:flex-row justify-center items-center w-full bg-slate-200">
+      <div className="flex justify-start md:justify-center  items-center w-full md:w-1/2 relative h-screen">
+        <img src="https://images.unsplash.com/photo-1512719994953-eabf50895df7?q=80&w=1929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-full absolute" />
+        <div className="text-white flex flex-col  gap-2 absolute p-10">
+          <h1 className="text-3xl font-bold italic">Welcome to CricMate</h1>
+          <ul className="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4">
+                {[
+                  "Live Score Updates",
+                  "Competitive Analysis",
+                  "Ball-by-Ball Commentary",
+                  "Stats and Insights",
+                ].map((item, index) => (
+                  <li key={index} className="flex items-center space-x-3">
+                    <div className="inline-flex items-center justify-center flex-shrink-0 w-5 h-5 bg-blue-500 rounded-full">
+                      <FaCheckCircle className="w-3.5 h-3.5 text-white" />
+                    </div>
+                    <span className="text-lg font-medium text-white">
+                      {item}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+          
+        </div>
+      </div>
+      <form className="flex flex-col gap-4 items-center justify-center w-full md:w-1/2 py-10 ">
+        <h1 className="text-2xl font-bold">SignUp to the exciting experience</h1>
+        <div className="flex gap-2 justify-center items-center ">
+        <p className="text-lg">Already have an account? </p>
+        <Link to="/login"> <p className="text-cyan-600 text-lg font-bold hover:text-orange-800">Login</p></Link>
+        </div>
         <input
           type="email"
-          className="border border-black p-2 rounded md:w-1/3 w-11/12"
+          className="border border-black p-2 rounded w-11/12"
           placeholder="Email"
           value={data.email}
           onChange={(e) => setData({ ...data, email: e.target.value })}
         />
 
         {showVerifyEmail && (
-          <div className="flex gap-2 w-11/12 md:w-1/3">
+          <div className="flex gap-2 w-11/12 ">
             <input
               type="text"
               className="border border-black p-2 rounded w-2/3"
@@ -93,7 +126,7 @@ function Signup() {
 
         <input
           type="password"
-          className="border border-black p-2 rounded md:w-1/3 w-11/12"
+          className="border border-black p-2 rounded w-11/12"
           placeholder="Password"
           value={data.password}
           onChange={(e) => setData({ ...data, password: e.target.value })}
@@ -117,6 +150,7 @@ function Signup() {
           </button>
         )}
       </form>
+      </div>
       <Footer />
     </>
   );
