6c46572d24036fb21ebbf7a323c1f3f93a870996
ankuraaryan5 <ankuraaryan5@gmail.com>
2024-10-20 09:03:25 +0530

    navbar made responsive

--- cricmate/src/Components/Navbar.jsx
+++ cricmate/src/Components/Navbar.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+{
+  /* <CiMenuBurger /> */
+}
 function Navbar() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className="flex justify-between p-5 text-white bg-slate-800 text-3xl font-bold text-center">
-      <div className="flex gap-1">
-        <img src="../../images/Logo.gif" alt="logo " width={40} height={40} />
-        <h1>CricMate</h1>
+    <div className="flex flex-col md:flex-row justify-between p-5 text-white bg-slate-800 text-3xl font-bold text-center w-full">
+      <div className="flex gap-2 w-full md:w-1/3 items-center md:justify-start justify-between">
+        <Link to={"/"}><img src="../../images/Logo.gif" alt="logo " width={40} height={40} /></Link>
+        <Link to={"/"}><h1>CricMate</h1></Link>
+      <div className="flex md:hidden">
+        <button
+          type="button"
+          className="text-gray-900"
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? (
+            <IoMdClose className="w-7 h-7 text-white" />
+          ) : (
+            <CiMenuBurger className="w-7 h-7 text-white" />
+          )}
+        </button>
       </div>
-      <div className="flex gap-5">
+      </div>
+      <div className="hidden md:flex gap-2 w-full md:w-1/3 items-center justify-center">
         <button className="text-lg font-normal">Live Matches</button>
         <button className="text-lg font-normal">Recent Updates</button>
         <button className="text-lg font-normal">Recent Results</button>
         <button className="text-lg font-normal">Upcoming Fixtures</button>
       </div>
-      <div className="flex gap-5">
+
+      <div className="hidden md:flex gap-2 w-full md:w-1/3 items-center md:justify-end justify-around">
         <button
           type="button"
           className="text-2xl border border-white py-1 px-2 rounded-lg hover:bg-white hover:text-slate-800"
@@ -24,6 +44,31 @@ function Navbar() {
           <Link to={"/signup"}>JoinUs</Link>
         </button>
       </div>
+      {expanded && (
+        <div className="md:hidden flex flex-col gap-2 w-full">
+          <div className=" flex flex-col gap-2 w-full ">
+          <div className="flex gap-2 w-full items-center justify-center">
+            <button className="text-lg font-normal">Live Matches</button>
+            <button className="text-lg font-normal">Recent Updates</button>
+            </div>
+          <div className="flex gap-2 w-full items-center justify-center">
+            <button className="text-lg font-normal">Recent Results</button>
+            <button className="text-lg font-normal">Upcoming Fixtures</button>
+            </div>
+          </div>
+          <div className="flex gap-2 w-full items-center justify-center">
+            <button
+              type="button"
+              className="text-2xl border border-white py-1 px-2 rounded-lg hover:bg-white hover:text-slate-800"
+            >
+              Login
+            </button>
+            <button className="text-2xl border border-white py-1 px-2 rounded-lg hover:bg-white hover:text-slate-800">
+              <Link to={"/signup"}>JoinUs</Link>
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
