b6302d5cd144776b562bebb4c454134580971503
ankuraaryan5 <ankuraaryan5@gmail.com>
2024-12-05 10:57:45 +0530

    Fetch Series location fixed

--- cricmate/src/App.jsx
+++ cricmate/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./Components/Hero";
 import Signup from "./Components/Authentication/Signup";
@@ -9,8 +10,36 @@ import Store from "./Components/Shop/Store";
 import MatchForm from "./Components/Crew/MatchForm";
 import ScoreForm from "./Components/Crew/ScoreForm";
 import SeriesForm from "./Components/Crew/SeriesForm";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getSeries, setLoading, setError } from "./store/scoreSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchSeriesList = async () => {
+      try {
+        dispatch(setLoading(true));
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/allSeries"
+        );
+        if (Array.isArray(response.data)) {
+          console.log(response.data);
+          dispatch(getSeries(response.data));
+        } else {
+          setError("Unexpected data format received.");
+        }
+      } catch (err) {
+        dispatch(setError(err.message || "Failed to fetch series list"));
+        setError(err.response?.data?.message || "Failed to fetch series list");
+      }
+      finally{
+        dispatch(setLoading(false));
+      }
+    };
+
+    fetchSeriesList();
+  }, []);
   const seriesData = useSelector((state) => state.series?.seriesData || []);
   console.log(seriesData);
   return (
--- cricmate/src/Components/Crew/Dashboard.jsx
+++ cricmate/src/Components/Crew/Dashboard.jsx
@@ -1,35 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Navbar from "../Navbar";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { getSeries, setLoading, setError } from "../../store/scoreSlice";
+
 
 function Dashboard() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const fetchSeriesList = async () => {
-      try {
-        dispatch(setLoading(true));
-        const response = await axios.get(
-          "http://localhost:4000/api/v1/allSeries"
-        );
-        if (Array.isArray(response.data)) {
-          console.log(response.data);
-          dispatch(getSeries(response.data));
-        } else {
-          setError("Unexpected data format received.");
-        }
-      } catch (err) {
-        dispatch(setError(err.message || "Failed to fetch series list"));
-        setError(err.response?.data?.message || "Failed to fetch series list");
-      }
-      finally{
-        dispatch(setLoading(false));
-      }
-    };
-
-    fetchSeriesList();
-  }, []);
+  
   return (
     <div className="flex flex-col justify-center items-center bg-black">
       <Navbar />
